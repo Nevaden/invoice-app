770e6085caa9c337d4198f6f48b035fc89749b66
Nevaden <aaron.k.dodson@gmail.com>
2022-10-26 18:58:39 -0400

    inital getData from service working.

--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -1,14 +1,26 @@
 import { Inject, Injectable } from '@angular/core';
 import { FirebaseApp, firebaseApp$ } from '@angular/fire/app';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { 
-    
+  URL: string = '';
+  firebaseURL: string = 'https://invoice-app-7e343-default-rtdb.firebaseio.com/';
+  JSON: string = '.json'
+  constructor(private http: HttpClient) { }
+
+  getAllData(): Observable<any>{
+    this.URL = `${this.firebaseURL}${this.JSON}`
+    return this.http.get(this.URL);
   }
+
+
+
+
 
 }
