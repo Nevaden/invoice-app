ab2db0e280be552f2ea354f3b5db6629b3c9ac9d
Nevaden <aaron.k.dodson@gmail.com>
2022-10-31 17:43:16 -0400

    mock post/patch

--- src/app/Components/view-invoice/view-invoice.component.ts
+++ src/app/Components/view-invoice/view-invoice.component.ts
@@ -13,16 +13,47 @@ export class ViewInvoiceComponent implements OnInit {
   id: any;
   invoice$: any;
   constructor(private store: Store, private route: ActivatedRoute) { }
-  
+  postTest = {
+    "id": "RT3080",
+    "createdAt": "2021-08-18",
+    "paymentDue": "2021-08-19",
+    "description": "Re-branding",
+    "paymentTerms": 1,
+    "clientName": "Jensen Huang",
+    "clientEmail": "jensenh@mail.com",
+    "status": "paid",
+    "senderAddress": {
+      "street": "19 Union Terrace",
+      "city": "London",
+      "postCode": "E1 3EZ",
+      "country": "United Kingdom"
+    },
+    "clientAddress": {
+      "street": "106 Kendell Street",
+      "city": "Sharrington",
+      "postCode": "NR24 5WQ",
+      "country": "United Kingdom"
+    },
+    "items": [
+      {
+        "name": "Brand Guidelines",
+        "quantity": 1,
+        "price": 1800.90,
+        "total": 1800.90
+      }
+    ],
+    "total": 1800.90
+  }
   ngOnInit(): void {
-    // this.store.select(selectInvoice).subscribe(params => {
-    //   console.log(params,"plz")
-    // })
-
     const id = this.route.snapshot.params['id'];
     this.invoice$ = this.store.select(oneInvoice(id))
+    
+    
+  }
 
-   
+
+  newInvoice(){
+    
   }
 
 }
